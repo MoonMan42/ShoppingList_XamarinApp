725661dd98dc434b4471e6050aeee1539074800a
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-09-28 23:46:25 -0400

    Setup new project and added  sqlite-net-pcl to main project.

---
+++ ShoppingList/ShoppingList/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ShoppingList.ViewModels;
+using ShoppingList.Views;
+using Xamarin.Forms;
+
+namespace ShoppingList
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
