aeff096bd4984f47054c15d6521817c1438337a1
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-09-28 23:52:49 -0400

    Removed login page, Added Product model

---
+++ ShoppingList/ShoppingList/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string StoreName { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
